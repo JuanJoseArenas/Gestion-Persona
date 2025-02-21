d081b6f87a0bb30c3e28acbf938f8cdcafd93faf
Juan Jose Arenas <juanjoseaq99@gmail.com>
2025-02-20 21:00:53 -0500

    Creacion de registro de persona

---
+++ src/app/empleado.ts
@@ -0,0 +1,11 @@
+export class Empleado {
+
+    id: number;
+    nombre: string;
+    apellido:string;
+    email:string;
+    tipoIdentificacion:string;
+    numeroIdentificacion:number;
+    direccion: string;
+    metodoDePago: string;
+}
